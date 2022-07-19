c76f26b74a3a389b6361476283873d94e955c580
Compass <ndphong165@gmail.com>
2022-07-19 22:43:12 +0700

    push bài 13

---
+++ BP.13/HW(LT1)/bai2.js
@@ -0,0 +1,11 @@
+
+// var arr = new Array(3,2,1,4,5,7,4,5,14,6,7);
+// var max = arr[0];
+// var index = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if(max < arr[i]) {
+//         max = arr[i]
+//         index = i
+//     }
+// }
+// console.log("Phần tử lớn nhất là: " + max + "; vị trí thứ: " + index)
